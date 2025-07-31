19c0d437a8cbe14d21d8a968f33a58337c479bc5
David9800-his <david.girard1@gmail.com>
2025-07-31 13:50:53 -0400

    Update agent_stats_hockey.py

--- agent_stats_hockey.py
+++ agent_stats_hockey.py
@@ -156,3 +156,8 @@ def stats():
 def logout():
     session.clear()
     return redirect(url_for("admin"))
+
+# --- Point d'entrée pour Render ---
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host="0.0.0.0", port=port)
