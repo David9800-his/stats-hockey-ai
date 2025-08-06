fb6a12481716ff09fb47bf6cd0293beb2266d255
David9800-his <david.girard1@gmail.com>
2025-08-06 00:10:55 -0400

    Correction COPILOT APP

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from config import Config
+from routes import main_routes, auth_routes, admin_routes
+from models.models import User
+import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-login = LoginManager(app)
-login.login_view = 'login'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'devkey')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from routes import main_routes, auth_routes, admin_routes
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'auth.login'
+
+# Register blueprints
 app.register_blueprint(main_routes.bp)
 app.register_blueprint(auth_routes.bp)
 app.register_blueprint(admin_routes.bp)
 
+# User loader for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ models/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
